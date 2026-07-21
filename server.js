fa9d67b579fc0ba880798a06242895406cf8c8d5
sonline-beep <melissasoaresonline@gmail.com>
2026-07-21 22:48:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,81 @@
 const express = require("express");
+const multer = require("multer");
 const fs = require("fs-extra");
 const path = require("path");
 const { v4: uuid } = require("uuid");
 
+const renderVideo = require("./utils/render");
+
 const app = express();
 
-app.use(express.json({ limit: "100mb" }));
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+
+const storage = multer.diskStorage({
+
+    destination: async (req, file, cb) => {
+
+        const jobId = req.jobId;
+
+        const pasta = path.join(__dirname, "temp", jobId);
+
+        await fs.ensureDir(pasta);
+
+        cb(null, pasta);
+
+    },
+
+    filename: (req, file, cb) => {
+
+        let nome = file.originalname;
+
+        if (file.fieldname === "audio") {
+
+            nome = "audio.mp3";
+
+        }
+
+        if (file.fieldname === "music") {
+
+            nome = "music.mp3";
+
+        }
+
+        if (file.fieldname === "videos") {
+
+            nome = Date.now() + "-" + file.originalname;
+
+        }
+
+        cb(null, nome);
+
+    }
+
+});
+
+app.use((req, res, next) => {
+
+    req.jobId = uuid();
+
+    next();
+
+});
+
+const upload = multer({
+    storage
+});
 
 app.get("/", (req, res) => {
+
     res.json({
+
         status: "ok",
+
         api: "Video Render API",
-        version: "3.0"
-    });
-});
 
-app.post("/render", async (req, res) => {
+        version: "1.0"
 
-    const jobId = uuid();
-
-    const pasta = path.join(__dirname, "temp", jobId);
-
-    await fs.ensureDir(pasta);
-
-    await fs.writeJson(
-        path.join(pasta, "dados.json"),
-        req.body,
-        { spaces: 2 }
-    );
-
-    res.json({
-        status: "ok",
-        jobId,
-        pastaCriada: pasta,
-        mensagem: "Dados recebidos com sucesso."
     });
 
 });
-
-app.listen(process.env.PORT || 8080);
